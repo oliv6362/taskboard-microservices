eed1b1abe6ab3290a83e674f82648f296d4816f3
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-12-11 12:43:45 +0100

    Add project files.

---
+++ ProjectService/Controllers/ProjectController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+
+    }
+}
---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
