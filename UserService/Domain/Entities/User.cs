143a884e82c9d7419676e916690dbac736be30ea
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-12-12 16:33:40 +0100

    feat(entity): added user entity

---
+++ UserService/Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserService.Domain.Entities
+{
+    public class User
+    {   
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
