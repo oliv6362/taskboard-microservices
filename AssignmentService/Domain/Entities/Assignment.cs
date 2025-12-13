7596a3054b76467bd03a5e6a43dceef34dad6db8
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-12-14 00:48:10 +0100

    feat(assignmentservice: added domain entity called Assignment

---
+++ AssignmentService/Domain/Entities/Assignment.cs
@@ -0,0 +1,20 @@
+﻿namespace AssignmentService.Domain.Entities
+{
+    public enum AssignmentStatus
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+
+    public class Assignment
+    {
+        public int AssignmentId { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public AssignmentStatus Status { get; set; } = AssignmentStatus.ToDo;
+        public int ProjectId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
