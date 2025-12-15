88eaf49a61d0821004cc94252150175144ee352d
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-12-15 14:29:31 +0100

    feat(assignmentservice): added fire-and-hope approach to the CreateAssignment Post Request

--- AssignmentService/Api/Controllers/AssignmentController.cs
+++ AssignmentService/Api/Controllers/AssignmentController.cs
@@ -1,19 +1,37 @@
 using AssignmentService.Api.Contracts.Dtos;
 using AssignmentService.Api.Contracts.Mappings;
 using AssignmentService.Application.Interfaces;
-using AssignmentService.Domain.Entities;
-using Azure.Core;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Net;
 
 namespace AssignmentService.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class AssignmentController(IAssignmentService assignmentService) : ControllerBase
     {
+        private static readonly RestClient restClient = new RestClient("http://project-service:8080");
+
         [HttpPost]
         public async Task<IActionResult> CreateAssignment([FromBody] CreateAssignmentRequest assignmentRequest, CancellationToken ct)
         {
+            //Fire-and-Hope
+            var request = new RestRequest($"/api/Project/{assignmentRequest.ProjectId}", Method.Get)
+            {
+                Timeout = TimeSpan.FromSeconds(2)
+            };
+
+            RestResponse response;
+            response = await restClient.ExecuteAsync(request, ct);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                return BadRequest("Project does not exist.");
+
+            if (!response.IsSuccessful)
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                    $"ProjectService returned {((int)response.StatusCode)} while validating ProjectId.");
+
             var assignment = assignmentRequest.ToEntity();
             var created = await assignmentService.CreateAssignment(assignment, ct);
             return Ok(created);
@@ -23,7 +41,7 @@ namespace AssignmentService.Api.Controllers
         public async Task<IActionResult> GetAssignmentById(int id, CancellationToken ct)
         {
             var assignment = await assignmentService.GetAssignmentById(id, ct);
-            return Ok(assignment);
+            return assignment is null ? NotFound() : Ok(assignment);
         }
 
         [HttpPut("status/{id:int}")]
