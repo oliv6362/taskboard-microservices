dec45fa83fad096bd6412d786bfc7cddc3e5056a
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-12-15 14:30:34 +0100

    feat(projectservice): added fire-and-hope approach to CreateProject post request

--- ProjectService/Api/Controllers/ProjectController.cs
+++ ProjectService/Api/Controllers/ProjectController.cs
@@ -1,27 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectService.Api.Contracts.Dtos;
-using ProjectService.Application.Interfaces;
 using ProjectService.Api.Contracts.Mappings;
+using ProjectService.Application.Interfaces;
+using RestSharp;
+using System.Net;
 
 namespace ProjectService.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProjectController(IProjectService projectService) : ControllerBase
     {
+        private static readonly RestClient restClient = new RestClient("http://user-service:8080");
+        
         [HttpPost]
         public async Task<IActionResult> CreateProject([FromBody] CreateProjectRequest projectRequest, CancellationToken ct)
         {
+            //Fire-and-Hope
+            var request = new RestRequest($"/api/User/{projectRequest.OwnerUserId}", Method.Get)
+            {
+                Timeout = TimeSpan.FromSeconds(2)
+            };
+
+            RestResponse response;
+            response = await restClient.ExecuteAsync(request, ct);
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+                return BadRequest("Owner user does not exist.");
+
+            if (!response.IsSuccessful)
+                return StatusCode(StatusCodes.Status503ServiceUnavailable,
+                    $"UserService returned {((int)response.StatusCode)} while validating OwnerUserId.");
+
             var project = projectRequest.ToEntity();
             var created = await projectService.CreateProject(project, ct);
             return Ok(created);
         }
 
-        [HttpGet]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetProjectById(int id, CancellationToken ct)
         {
             var project = await projectService.GetProjectById(id, ct);
-            return Ok(project);
+            return project is null ? NotFound() : Ok(project);
         }
     }
 }
